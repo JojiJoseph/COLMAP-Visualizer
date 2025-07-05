68bfe5ef72a484ee05408b5be2ec8614887306ee
Joji <jojijoseph@iisc.ac.in>
2025-07-05 16:02:53 +0530

    Add sliders to rotate

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(central_widget)
 
         self.renderer = PCDRenderer(width=800, height=600)
-        self.renderer.load(self.colmap_project.points3D, self.colmap_project.point3D_colors)
+        self.renderer.load(self.colmap_project.points3D, self.colmap_project.point3D_colors / 255.0,)
 
         # Sliders for x, y, z
         self.slider_x = QSlider(Qt.Orientation.Horizontal, self)
@@ -64,6 +64,37 @@ class MainWindow(QMainWindow):
         self.slider_z.setTickPosition(QSlider.TickPosition.TicksBelow)
         self.slider_z.setToolTip("View Z (cm)")
 
+        # Sliders for roll, pitch, yaw
+        self.slider_roll = QSlider(Qt.Orientation.Horizontal, self)
+        self.slider_roll.setRange(-180, 180)
+        self.slider_roll.setValue(0)
+        self.slider_roll.setTickInterval(30)
+        self.slider_roll.setTickPosition(QSlider.TickPosition.TicksBelow)
+        self.slider_roll.setToolTip("Roll (deg)")
+
+        self.slider_pitch = QSlider(Qt.Orientation.Horizontal, self)
+        self.slider_pitch.setRange(-180, 180)
+        self.slider_pitch.setValue(0)
+        self.slider_pitch.setTickInterval(30)
+        self.slider_pitch.setTickPosition(QSlider.TickPosition.TicksBelow)
+        self.slider_pitch.setToolTip("Pitch (deg)")
+
+        self.slider_yaw = QSlider(Qt.Orientation.Horizontal, self)
+        self.slider_yaw.setRange(-180, 180)
+        self.slider_yaw.setValue(0)
+        self.slider_yaw.setTickInterval(30)
+        self.slider_yaw.setTickPosition(QSlider.TickPosition.TicksBelow)
+        self.slider_yaw.setToolTip("Yaw (deg)")
+
+        rot_slider_layout = QHBoxLayout()
+        rot_slider_layout.addWidget(QLabel("Roll:"))
+        rot_slider_layout.addWidget(self.slider_roll)
+        rot_slider_layout.addWidget(QLabel("Pitch:"))
+        rot_slider_layout.addWidget(self.slider_pitch)
+        rot_slider_layout.addWidget(QLabel("Yaw:"))
+        rot_slider_layout.addWidget(self.slider_yaw)
+        layout.addLayout(rot_slider_layout)
+
         slider_layout = QHBoxLayout()
         slider_layout.addWidget(QLabel("X:"))
         slider_layout.addWidget(self.slider_x)
@@ -77,8 +108,7 @@ class MainWindow(QMainWindow):
         self.elapsed_seconds = 0
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_timer)
-        self.timer.start(10)  # 1 second interval
-
+        self.timer.start(10)  # 100 ms interval
     def update_timer(self):
         self.elapsed_seconds += 1
         self.label.setText(f"Elapsed time: {self.elapsed_seconds} s")
@@ -93,6 +123,12 @@ class MainWindow(QMainWindow):
         viewmat[0, 3] = self.slider_x.value() / 100.0  # Convert cm to m
         viewmat[1, 3] = self.slider_y.value() / 100.0
         viewmat[2, 3] = self.slider_z.value() / 100.0
+
+        roll = np.radians(self.slider_roll.value())
+        pitch = np.radians(self.slider_pitch.value())
+        yaw = np.radians(self.slider_yaw.value())
+        rotation = scipy_transform.Rotation.from_euler('xyz', [roll, pitch, yaw], degrees=False)
+        viewmat[:3, :3] = rotation.as_matrix()
 
         arr = self.renderer.render(K, viewmat=viewmat, near=0.1, far=100.0)
         arr = arr.copy()# / 255.0
